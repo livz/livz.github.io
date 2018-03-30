9f865e94af93899a420acd24a778848f4e04b385
livz <liviu.itoafa@gmail.com>
2018-03-30 16:04:52 +0100

    Update keyboard.js

--- assets/scripts/keyboard.js
+++ assets/scripts/keyboard.js
@@ -1,4 +1,31 @@
 // Navigate previous/next entry VIM style
+function entriesNav(key) {
+  var mainContent = document.getElementById("main_content");
+  var currentList = mainContent.getElementsByTagName("ul")[0];
+  var currentClass = currentList.className
+  var listEntries = [].slice.call(currentList.getElementsByTagName("li"));
+
+  var idx = -1; 
+
+  var currentfocus = document.activeElement; 
+
+  // get current post index
+  if ( currentfocus.parentElement.parentElement.className === currentClass ) {
+    idx = listEntries.indexOf(currentfocus.parentElement)
+  } 
+
+  // Adjust post index
+  if ( key === 'j' && idx < listEntries.length - 1 ) {
+    idx++;
+  } else if (key === 'k' && idx > 0 ) {
+    idx--;
+  }
+
+  // Move focus to the link
+  listEntries[idx].getElementsByTagName("a")[0].focus();
+}
+
+// Go to previous/next post/tip/...
 function blogNav(key) {
   if ( key === 'h' ) {
     var navPrev = document.querySelectorAll(".PageNavigation .prev")
@@ -9,12 +36,15 @@ function blogNav(key) {
   }
 }
 
+// Bind keys
 Mousetrap.bind({
-  'g h': function() {	window.location.href = "/"; },
-  'g b': function() {	window.location.href = "/blog"; },
+  'g h': function() { window.location.href = "/"; },
+  'g b': function() { window.location.href = "/blog"; },
   'g c': function() { window.location.href = "/cheat-sheets"; },
   'g t': function() { window.location.href = "/tips"; },
   'g a': function() { window.location.href = "/about"; },
-  'h': function() { blogNav('h'); },
-  'l': function() { blogNav('l'); },
+  'h':   function() { blogNav('h'); },
+  'l':   function() { blogNav('l'); },
+  'j':   function() { entriesNav('j'); },
+  'k':   function() { entriesNav('k'); },
 })
