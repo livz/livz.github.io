9b045ade90f4a3c839fdfa8364be07158b57e84b
livz <liviu.itoafa@gmail.com>
2018-03-30 13:38:58 +0100

    Create keyboard.js

---
+++ assets/scripts/keyboard.js
@@ -0,0 +1,7 @@
+Mousetrap.bind({
+	'g h': function() {	window.location.href = "/"; },
+	'g b': function() {	window.location.href = "/blog"; },
+  'g c': function() { window.location.href = "/cheat-sheets"; },
+  'g t': function() { window.location.href = "/tips"; },
+  'g a': function() { window.location.href = "/about"; },
+})
