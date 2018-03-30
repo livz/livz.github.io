2a7483fee2439aa1ec32ca78d53f32546eea92d1
livz <liviu.itoafa@gmail.com>
2018-03-30 14:07:52 +0100

    Update keyboard.js

--- assets/scripts/keyboard.js
+++ assets/scripts/keyboard.js
@@ -10,11 +10,11 @@ function blogNav(key) {
 }
 
 Mousetrap.bind({
-	'g h': function() {	window.location.href = "/"; },
-	'g b': function() {	window.location.href = "/blog"; },
-	'g c': function() { window.location.href = "/cheat-sheets"; },
-	'g t': function() { window.location.href = "/tips"; },
-	'g a': function() { window.location.href = "/about"; },
-	'h': function() { blogNav('h'); },
-	'l': function() { blogNav('l'); },
+  'g h': function() {	window.location.href = "/"; },
+  'g b': function() {	window.location.href = "/blog"; },
+  'g c': function() { window.location.href = "/cheat-sheets"; },
+  'g t': function() { window.location.href = "/tips"; },
+  'g a': function() { window.location.href = "/about"; },
+  'h': function() { blogNav('h'); },
+  'l': function() { blogNav('l'); },
 })
